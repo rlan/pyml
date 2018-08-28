c732ac63a9bf8f80bc8356ec6fd8aedccdea5a21
Rick Lan <rlan@users.noreply.github.com>
2018-08-28 10:56:50 +0900

    Add Image module

---
+++ Image.py
@@ -0,0 +1,51 @@
+"""
+Image related routines
+"""
+
+import numpy as np
+
+def floatToUint8(img, scale = None, minmax = None, scretch = False):
+  """Convert floating-point matrix to 8-bit matrix for writing as JPG file
+
+  Parameters
+  ----------
+  img : ndarray
+      Input matrix
+  scale : float
+      If None, no scaling is applied.
+  minmax : tuple of float
+      If None, no scaling is applied. Otherwise input is scaled such that it fits
+      in the result [min, max].
+  scretch : boolean
+      If true, scretch min and max of input signal to occupy the 8-bit output.
+
+  Returns
+  -------
+  ndarray
+      Image
+  """
+  #print("shape", img.shape)
+
+  if scale != None:
+    img = img * scale
+
+  if minmax != None:
+    amax = np.amax(img)
+    amin = np.amin(img)
+    scale = (minmax[1]-minmax[0]) / (amax - amin)
+    img = (img-amin) * scale
+
+  if scretch:
+    amax = np.amax(img)
+    amin = np.amin(img)
+    #print("amax", amax, "amin", amin)
+    if amax != amin:
+      scale = 255.0/(amax-amin)
+      img = (img-amin) * scale
+
+  img = np.clip(img, 0.0, 255.0)
+  return img.astype(dtype='uint8')
+
+
+if __name__ == "__main__":
+  # TODO need tests
